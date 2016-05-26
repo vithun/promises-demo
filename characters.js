5c9ad0b0eb32ae497a9af0d7a68f4451898bd23f
Vithun Kumar Gajendra <vgajendra@expedia.com>
2016-05-26 16:35:36 +0100

    Story with callbacks

--- characters.js
+++ characters.js
@@ -5,26 +5,30 @@ var Ship = function (element) {
     this.passengers = [];
 };
 
-Ship.prototype.addPassenger = function (passenger) {
-    passenger.element.style.display = 'block';
+Ship.prototype.addPassenger = function (passenger, callback) {
+    passenger.element.addEventListener('transitionend', callback);
+    passenger.element.style.opacity = '1';
     this.passengers.push(passenger);
 };
 
-Ship.prototype.sail = function () {
+Ship.prototype.sail = function (callback) {
+    this.element.addEventListener('transitionend', callback);
     this.element.classList.add('sail');
     this.passengers.forEach(function (passenger) {
         passenger.element.classList.add('sail');
     });
 };
 
-Ship.prototype.tilt = function () {
+Ship.prototype.tilt = function (callback) {
+    this.element.addEventListener('transitionend', callback);
     this.element.classList.add('tilt');
     this.passengers.forEach(function (passenger) {
         passenger.element.classList.add('float');
     });
 };
 
-Ship.prototype.drown = function () {
+Ship.prototype.drown = function (callback) {
+    this.element.addEventListener('transitionend', callback);
     this.element.classList.add('drown');
 };
 
--- story.js
+++ story.js
@@ -1,3 +1,17 @@
 var titanic = new Ship(document.querySelector('.titanic'));
 var jack = new Passenger(document.querySelector('.jack'));
 var rose = new Passenger(document.querySelector('.rose'));
+
+
+function story() {
+    titanic.addPassenger(jack, function () {
+        titanic.addPassenger(rose, function () {
+            titanic.sail(function () {
+                titanic.tilt(function () {
+                    titanic.drown();
+                    jack.drown();
+                });
+            });
+        });
+    });
+}
