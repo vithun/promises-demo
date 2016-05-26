cb5fba9e38dcd38ec9f8914a9aeae160e1cacea2
Vithun Kumar Gajendra <vgajendra@expedia.com>
2016-05-26 17:02:54 +0100

    Use this binding

--- story.js
+++ story.js
@@ -5,9 +5,9 @@ var rose = new Passenger(document.querySelector('.rose'));
 
 function story() {
     Q.all([titanic.addPassenger(jack), titanic.addPassenger(rose)])
-        .then(function () { return titanic.sail(); })
-        .then(function () { return titanic.tilt(); })
-        .then(function () { return titanic.drown(); })
-        .then(function () { return jack.drown(); })
+        .then(titanic.sail.bind(titanic))
+        .then(titanic.tilt.bind(titanic))
+        .then(titanic.drown.bind(titanic))
+        .then(jack.drown.bind(jack))
         .catch(function (error) { console.error(error); });
 }
